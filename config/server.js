09502e5f915e14d6b7bbd34ff211ca165208fe93
Changsik Yoon <dbs2636@gmail.com>
2022-01-21 20:14:48 +0900

    project: Downgrade to v3

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '99dce506613c5e395af85ad3effd0798'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a04f8dda-808a-4860-9326-9c1d79515bba'
+};
